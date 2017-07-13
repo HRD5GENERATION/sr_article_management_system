6113910d97e54973c76839b2fe3ade0763e3963d
Phearun Rath <rathphearun168@gmail.com>
2017-07-13 08:22:46 +0700

    updated: database initialization

--- src/main/java/com/sr/configuration/DatabaseConfiguration.java
+++ src/main/java/com/sr/configuration/DatabaseConfiguration.java
@@ -3,14 +3,19 @@ package com.sr.configuration;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
 @PropertySource("classpath:ams.properties")
@@ -27,6 +32,7 @@ public class DatabaseConfiguration {
 		dataSource.setUrl(env.getProperty("dev.datasource.url"));
 		dataSource.setUsername(env.getProperty("dev.datasource.username"));
 		dataSource.setPassword(env.getProperty("dev.datasource.password"));
+		this.initializeDatabase(dataSource);
 		return dataSource;
 	}
 	
@@ -38,6 +44,7 @@ public class DatabaseConfiguration {
 		dataSource.setUrl(env.getProperty("pro.datasource.url"));
 		dataSource.setUsername(env.getProperty("pro.datasource.username"));
 		dataSource.setPassword(env.getProperty("pro.datasource.password"));
+		this.initializeDatabase(dataSource);
 		return dataSource;
 	}
 	
@@ -51,7 +58,10 @@ public class DatabaseConfiguration {
 		return builder.build();
 	}
 	
-	/*@Value("classpath:db/schema.sql")
+	
+	//TODO: Database Initialization
+	
+	@Value("classpath:db/schema.sql")
 	private Resource schema;
 	
 	@Value("classpath:db/data.sql")
@@ -60,5 +70,5 @@ public class DatabaseConfiguration {
 	private void initializeDatabase(DataSource dataSource) {
 		DatabasePopulator populator = new ResourceDatabasePopulator(schema, data);
 		DatabasePopulatorUtils.execute(populator, dataSource);
-	}*/
+	}
 }
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,3 +1,21 @@
+DROP TABLE IF EXISTS tbarticle;
+DROP TABLE IF EXISTS tbcategory;
+
+
+CREATE TABLE tbcategory(
+	id serial primary key,
+    name VARCHAR(50)
+);
+
+CREATE TABLE tbarticle(
+	id serial primary key,
+	title VARCHAR(100),
+	description VARCHAR(500),
+	thumbnail VARCHAR(500),
+	category_id int REFERENCES tbcategory(id)
+);
+
+/*
 CREATE TABLE tbcategory(
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(50) 
@@ -10,3 +28,4 @@ CREATE TABLE tbarticle(
 	thumbnail VARCHAR(500),
 	category_id INT REFERENCES tbcategory(id)
 );
+*/
